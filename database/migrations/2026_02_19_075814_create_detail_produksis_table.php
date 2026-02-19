edd1c3f0d00ead3ba943d92a7b00fd7268369165
Verbatama <johanwidya28@gmail.com>
2026-02-19 16:19:22 +0700

    1. Create Migrations

---
+++ database/migrations/2026_02_19_075814_create_detail_produksis_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('detail_produksis', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('produksi_id')->references('id')
+                                            ->on('produksis');
+            $table->foreignId('bahan_id')->references('id')
+                                         ->on('bahan_bakus');
+            $table->integer('qty_bahan')->unsigned();
+            $table->decimal('biaya',15,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('detail_produksis');
+    }
+};
